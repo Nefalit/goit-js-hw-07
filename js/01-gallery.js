71f039eb033f1cce168ac82ab24ee2f8c51d3c54
Nefalit <nefalit7@gmail.com>
2022-05-23 20:49:28 +0300

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+function renderGallaryList(array) {
+  const listImgMarkup = array
+    .map(({ preview, original, description } = {}) => {
+      return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", listImgMarkup);
+}
+
+renderGallaryList(galleryItems);
+
+let imgSrc = {};
+galleryEl.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const currentImg = ev.target;
+  if (currentImg.nodeName !== "IMG") {
+    return;
+  }
+  imgSrc = basicLightbox.create(
+    `<div class="modal">
+    <img src="${ev.target.dataset.source}" width="800" height="600"></img>
+    </div>`
+  );
+  imgSrc.show();
+  document.addEventListener("keydown", closeImg);
+});
+
+function closeImg(ev) {
+  if (ev.code === "Escape" && imgSrc.visible()) {
+    imgSrc.close();
+    document.removeEventListener("keydown", closeImg);
+  }
+}
 
 console.log(galleryItems);
